5c551045b67ed05cafc94787149cf8facc43e214
飯塚啓介 <26keisuke@gmail.com>
2020-03-09 22:51:21 -0400

    changed heroku config

--- dist/config/keys.js
+++ dist/config/keys.js
@@ -4,5 +4,6 @@ if (process.env.NODE_ENV === "production") {
   console.log(process.env);
   module.exports = require("./prod");
 } else {
+  console.log(process.env);
   module.exports = require("./dev");
 }
--- src/config/keys.js
+++ src/config/keys.js
@@ -2,5 +2,6 @@ if (process.env.NODE_ENV === "production") {
     console.log(process.env)
     module.exports = require("./prod");
 } else {
+    console.log(process.env)
     module.exports = require("./dev");
 }
